978a01908cae2da1a8490386024c9cbf4c73da8e
poinkpolkadots <olkadotspinkp@gmail.com>
2026-06-10 00:19:48 -0400

    added -y to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 # obvious
-sudo apt install vim
+sudo apt install vim -y
 
 # ssh
 sudo apt install openssh-server -y
-sudo systemctl enable ssh
-sudo systemctl start ssh
-ssh-import-id gh:pinkpolkadots
+sudo systemctl enable ssh -y
+sudo systemctl start ssh -y
+ssh-import-id gh:pinkpolkadots -y
 
 # fan control, need to change the configuration
-sudo apt install mbpfan
-sudo service mbpfan start
-sudo wget https://github.com/linux-on-mac/mbpfan/raw/refs/heads/master/mbpfan.upstart
-sudo cp mbpfan.upstart /etc/init/mbpfan.conf
-sudo start mbpfan
+sudo apt install mbpfan -y
+sudo service mbpfan start -y
+sudo wget https://github.com/linux-on-mac/mbpfan/raw/refs/heads/master/mbpfan.upstart -y
+sudo cp mbpfan.upstart /etc/init/mbpfan.conf -y
+sudo start mbpfan -y
 
 # docker
 # Add Docker's official GPG key:
-sudo apt update
-sudo apt install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+sudo apt update -y
+sudo apt install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyringss -y
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc -y
+sudo chmod a+r /etc/apt/keyrings/docker.asc -y
 
 #Add the repository to Apt sources:
 sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
@@ -33,14 +33,14 @@ Components: stable
 Architectures: $(dpkg --print-architecture)
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt update -y
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # GitHub login
 # gh auth login
 
 # nginx
-sudo apt install nginx
+sudo apt install nginx -y
 # sudo systemctl start nginx
 # sudo systemctl enable nginx
 
